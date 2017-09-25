27a48f0df22348ba5ffe324497691d4fcd2d3eb2
jiangjun <jiangjun@github.io>
2017-09-25 23:43:43 +0800

    process

--- process/child_process.js
+++ process/child_process.js
@@ -1,4 +1,4 @@
-//子进程
+//子进程   当要使用系统命令时应该用spawn()，要确保node能访问这一命令
 /*
 var spawn = require('child_process').spawn
 var ping = spawn('ping',['bbc.co.uk'])
@@ -12,7 +12,7 @@ ping.on('exit',function(code,signal){
 ping.kill('SIGINT')
   */
 
-//与子进程通信
+//与子进程通信  fork()用于创建新的进程
 var fork = require('child_process').fork
 var child = fork(__dirname+'/child.js')
 child.on('message',function(m){
